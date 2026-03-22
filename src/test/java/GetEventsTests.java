460b3b3ce4659c29bb98ca14b8ed3d8c53a1822c
Dijana Spasevska <spasevska.dijana@gmail.com>
2026-03-22 21:44:48 +0100

    final project

--- src/test/java/GetEventsTests.java
+++ src/test/java/GetEventsTests.java
@@ -1,14 +1,40 @@
+import client.EVPrimeClient;
+import data.SignUpLoginDataFactory;
+import io.restassured.response.Response;
+import models.request.SignUpLoginRequest;
+import models.response.LoginResponse;
+import models.response.SignUpResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import util.Configuration;
 
 import java.sql.SQLException;
 
-public class GetEventsTests {
+import static objectBuilder.SignUpLoginObjectBuilder.createBodyForSignUpLogin;
+
+public class GetAllEventsTests {
+
+    private SignUpLoginRequest signUpRequest;
+    private LoginResponse loginResponseBody;
 
     @Before
     public void setUp() {
+
+        signUpRequest = new SignUpLoginDataFactory(createBodyForSignUpLogin())
+                .setEmail(Configuration.USER_MAIL)
+                .setPassword(Configuration.USER_PASSWORD)
+                .createRequest();
+
+        // sign up
+        new EVPrimeClient()
+                .signUp(signUpRequest);
+
         // login
+        Response loginResponse = new EVPrimeClient()
+                .login(signUpRequest);
+
+        loginResponseBody = loginResponse.body().as(LoginResponse.class);
         // create event (get if from the event)
     }
 
